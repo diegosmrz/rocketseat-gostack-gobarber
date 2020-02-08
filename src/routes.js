0e5160b1e32258877e0d0ba5b818de1aae7031ca
diegomrz <diegomrz@outlook.com>
2020-02-08 20:03:20 -0300

    config-database02 - 'Criando loader de models'

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
-const { Router } = require('express');
+import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello world!' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego Martinez',
+    email: 'diegomrz@outlook.com',
+    password_hash: '12345678',
+  });
+
+  return res.json(user);
 });
 
-module.exports = routes;
+export default routes;
